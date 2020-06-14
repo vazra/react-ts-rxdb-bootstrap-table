1f9750b6e6cd9ce267aac0e141a47eacfa7034e7
Ameen Rashad <ameenrashadv@gmail.com>
2020-06-14 08:36:45 +0530

    Updates

---
+++ precache-manifest.93a16be7d134d295e222731c8baf27a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a3668c40dd1c0703521cee6fa19a4b",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/index.html"
+  },
+  {
+    "revision": "b3d2aa84c663105b8fae",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/css/2.7ef9b196.chunk.css"
+  },
+  {
+    "revision": "f46cabfcc32d6c68036b",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b3d2aa84c663105b8fae",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/js/2.9962146d.chunk.js"
+  },
+  {
+    "revision": "1ae4ce903cf53cb8a5752c001e7ea61f",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/js/2.9962146d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f46cabfcc32d6c68036b",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/js/main.5daab1b8.chunk.js"
+  },
+  {
+    "revision": "3547b01c4a43e2373c9c",
+    "url": "/example-react-ts-rxdb-bootstrap-table2/static/js/runtime-main.347e5de7.js"
+  }
+]);
